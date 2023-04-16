4d074d2438c9c641568ad6a50f420414e3f4ad2d
Seb Ringrose <sebringrose@gmail.com>
2023-04-16 17:37:46 +0100

    fix: resize image fileUrl

--- imagemagick_deno/handlers/resize_to_webp.ts
+++ imagemagick_deno/handlers/resize_to_webp.ts
@@ -7,14 +7,14 @@ import {
 
 await initializeImageMagick()
 
-export const resizableImage =  (fileRoute: string, resolutionMap: Map<string, number>) => async (ctx: RequestContext) => {
+export const resizableImage =  (fileUrl: URL, resolutionMap: Map<string, number>) => async (ctx: RequestContext) => {
   const params = new URL(ctx.request.url).searchParams
   const res = resolutionMap.get(params.get("res") || "")
 
-  return await staticHandler(new URL(`../${fileRoute}`, import.meta.url), {
+  return await staticHandler(fileUrl, {
     transform: (contents) => {
       if (!res) return contents
-      console.log("Resizing " + fileRoute + " to res: " + res)
+      console.log("Resizing " + fileUrl + " to res: " + res)
       
       return new Promise(resolve => ImageMagick.read(contents, (img) => {
         img.resize(res, res)
